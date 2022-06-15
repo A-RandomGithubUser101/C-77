dc576ab6d3eac8b3abe60aa28b01c9d6ce91340f
A-RandomGithubUser101 <96635244+A-RandomGithubUser101@users.noreply.github.com>
2022-06-15 11:52:17 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+console.log("Hello");
+var bob="Friend";
+console.log(bob);
+
+var philosophy="THOUGHTS";
+console.log(philosophy);
+
+var join=bob+" "+philosophy;
+console.log(join);
+
+var length=join.length;
+console.log(length);
+
+var replace=bob.replace("Friend","darkness");
+console.log(replace);
+
+var capitalize=bob.toUpperCase();
+console.log(capitalize);
+
+var lowercase=philosophy.toLowerCase();
+console.log(lowercase);
+
+var maxnumber=Math.max(30000004,85543233432920392,3342456,11049,1,-53,-52.999999999999999999999999999999999999999999);
+console.log(maxnumber);
+
+var minnumber=Math.min(17, 252,-3,-17/32, -4/9);
+console.log(minnumber);
+
+var sqrt=Math.sqrt(441);
+console.log(sqrt)
+
+var power=Math.pow(17,2);
+console.log(power);
